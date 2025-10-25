3d023932a6f51a56d50820ad951c835de3171869
Rohan Kumar Sahoo <rohankumarsahoo2001@gmail.com>
2025-10-25 13:13:58 +0530

    Latest Changes Added

--- src/main/java/com/springboot/ecom_proj/controller/ProductController.java
+++ src/main/java/com/springboot/ecom_proj/controller/ProductController.java
@@ -90,4 +90,12 @@ public class ProductController {
             return new ResponseEntity<>("Product Not Found!", HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        System.out.println("Keyword : "+keyword);
+        List<Product> products = service.searchProducts(keyword);
+
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
--- src/main/java/com/springboot/ecom_proj/repository/ProductRepo.java
+++ src/main/java/com/springboot/ecom_proj/repository/ProductRepo.java
@@ -2,12 +2,20 @@ package com.springboot.ecom_proj.repository;
 
 import com.springboot.ecom_proj.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
-
+    @Query("SELECT p from Product p WHERE "+
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword ,'%')) OR "+
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword ,'%')) OR "+
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword ,'%')) OR "+
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword ,'%')) ")
+    List<Product> searchProducts(String keyword);
 }
